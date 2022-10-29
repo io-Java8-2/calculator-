5576af995bd2e1080c311f12fc6d4652072095c6
Eryk <eryk@Zipcoders-MacBook-Pro-2.local>
2022-10-29 18:00:31 -0400

    add memory class

--- src/main/java/com/zipcodewilmington/scientificcalculator/Memory.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Memory.java
@@ -1,2 +1,14 @@
 package com.zipcodewilmington.scientificcalculator;
 
+public class Memory{
+
+    public static void saveToMemory(double currentMemoryValue, double displayNumber) { // saves current mem value
+        currentMemoryValue = displayNumber;
+    }
+    public static void resetMemory(double currentMemoryValue){ //reset memory
+        currentMemoryValue = 0;
+    }
+    public static double recallMemory(double currentMemoryValue){
+        return currentMemoryValue;
+    }
+}
