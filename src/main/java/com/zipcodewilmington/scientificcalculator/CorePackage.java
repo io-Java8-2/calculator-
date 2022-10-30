f7668f7be686afc4f26b9d1a448c87d7559483ac
Eryk <eryk@Zipcoders-MBP-2.fios-router.home>
2022-10-30 13:33:16 -0400

    add core methods

--- src/main/java/com/zipcodewilmington/scientificcalculator/CorePackage.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/CorePackage.java
@@ -4,4 +4,13 @@ public class CorePackage {
     public static double addition(double x, double y){
         return x + y;
     }
+    public static double sqrt(double x){ return x =  Math.sqrt(x);}
+
+    public static double sq(double x){return x =  x * x;}
+
+    public static double raisePower(double x, double y){return x =  Math.pow(x, y);}
+
+    public static double inverse(double x){return x = 1/x;}
+
+    public static double invertSign(double x){return x = x * -1;}
 }
