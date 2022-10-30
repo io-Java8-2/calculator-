0a6b6234bcce86d352c089a48973feaea76caaaf
Eryk <eryk@Zipcoders-MBP-2.fios-router.home>
2022-10-30 14:23:00 -0400

    none

---
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestCorePackage.java
@@ -0,0 +1,6 @@
+package com.zipcodewilmington.scientific_calculator;
+import com.zipcodewilmington.scientificcalculator.Trig;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.text.DecimalFormat;
+
